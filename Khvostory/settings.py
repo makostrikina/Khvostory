f297bad5b3b5490c94e165faf48c8bd7e0f40176
makostrikina <makostrikina@edu.hse.ru>
2026-05-18 23:16:02 +0500

    fix allowed hosts

--- Khvostory/settings.py
+++ Khvostory/settings.py
@@ -35,12 +35,11 @@ SECRET_KEY = '=wh+5a03pyst!shn4e8yb=iki(#lfqpb)362a6wn75mgqg0p3b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
-
 ALLOWED_HOSTS = [
+    'localhost',
     '127.0.0.1',
-    'localhost'
-    ]
+    '.onrender.com'
+]
 
 
 # Application definition
